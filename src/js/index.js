54b036178009115e0e9c27d27b62ec92bbd8a595
don <1776565321@qq.com>
2019-02-15 17:33:33 +0800

    配置devServer热更新

---
+++ src/js/index.js
@@ -0,0 +1,1 @@
+document.body.innerHTML = "this is a demo of webpack"
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,21 @@
+const path = require('path');
 module.exports = {
+  mode: 'development',
   entry: {
-
+    index: './src/js/index.js'
   },
   output: {
-
+    path: path.resolve(__dirname, 'dist'),
+    filename: "[name].js"
   },
   module: {
 
   },
   plugins: [],
   devServer: {
-    
+    contentBase: path.resolve(__dirname, 'dist'),
+    compress: true,
+    host: 'localhost',
+    port: 1717
   }
 }
